25e91e9b04e5d35eb085604f1b7394bf79dc3895
Yasir Ejaz <ejazyasir1974501@gmail.com>
2024-05-02 11:37:31 +0500

    first commit

---
+++ lib/utils/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class MyColors {
+  static Color primaryColor = Colors.yellow.shade400;
+  static const List<Color> primaryGradientColor = [
+    Color.fromARGB(255, 199, 190, 3),
+    Colors.black,
+  ];
+}
